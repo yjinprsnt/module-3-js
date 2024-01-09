524f54293b15a84162341c45f85835fc5462bcdd
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-09 19:25:08 +0200

    add 1 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -701,3 +701,12 @@
 // }
 
 // includes([1, 2, 3, 4, 5], 3);
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
