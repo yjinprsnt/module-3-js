47a56dbdb246b53bfd27faeeca54679cb68f5cd6
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-09 19:25:26 +0200

    add 2 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -710,3 +710,16 @@
 //   tags: ['premium', 'promoted', 'top'],
 // };
 
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
