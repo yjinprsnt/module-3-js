73f21285cd03333006f2dc0f31514226fbb3f9c6
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-09 19:28:06 +0200

    add 6 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -775,3 +775,22 @@
 // const aptTags = apartment['tags'];
 // // Change code above this line
 
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// // Change code below this line
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Henry Sibola';
+// apartment.tags.push('trusted');
+
