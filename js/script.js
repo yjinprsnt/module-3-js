4c0cc609eb3c8e18fae6e8862fda349fbeff4bd0
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-09 19:28:50 +0200

    add 7 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -794,3 +794,24 @@
 // apartment.owner.name = 'Henry Sibola';
 // apartment.tags.push('trusted');
 
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Henry Sibola',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//   country: 'Jamaica',
+//   city: 'Kingston',
+// };
+
