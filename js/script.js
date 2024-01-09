9bc32f279dc3c34afb5e16edde0121194342c161
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-09 19:26:20 +0200

    add 4 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -738,3 +738,25 @@
 // const aptTags = apartment.tags;
 // // Change code above this line
 
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[2];
+// // Change code above this line
+
