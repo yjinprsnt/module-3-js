a4627c1789b00a907d1133a2025e757d17b04f41
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-09 19:26:49 +0200

    add 5 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -760,3 +760,18 @@
 // const lastTag = apartment.tags[2];
 // // Change code above this line
 
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+// // Change code below this line
+// const aptRating = apartment['rating'];
+// const aptDescr = apartment['descr'];
+// const aptPrice = apartment['price'];
+// const aptTags = apartment['tags'];
+// // Change code above this line
+
