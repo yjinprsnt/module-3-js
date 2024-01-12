699edec756e7e4a03f029d1f18410ac27d6d0c45
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-12 20:24:10 +0200

    add 11 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -849,3 +849,22 @@
 //   values.push(apartment[key]);
 // }
 
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Spacious apartment in the city center';
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+//   // Change code above this line
+// }
+
