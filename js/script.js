518c6fc5d1621cc82a5092b8c895e8eb65a8cc6b
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-12 22:43:53 +0200

    add 13 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -880,3 +880,18 @@
 //   return propCount;
 // }
 
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// console.log(keys);
+
+// for (const key of keys) {
+//   values.push(apartment[key]);
+//   // console.log(values);
+// }
+
