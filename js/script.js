da29effedff4f21db5b07a45509401f20e820211
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-12 20:22:48 +0200

    add 8 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -815,3 +815,14 @@
 //   city: 'Kingston',
 // };
 
+// const name = 'Repair Droid';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
+
+// const product = {
+//   name,
+//   price,
+//   image,
+//   tags,
+// };
