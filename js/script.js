091485ca8e0375607bda90cee0388a3160d1bd77
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-12 20:23:39 +0200

    add 10 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -835,3 +835,17 @@
 //   [passwordInputName]: 'jqueryismyjam',
 // };
 
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// let all = [];
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+
