402f125a43be3fc95518e93381b132b99402354f
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-12 22:44:14 +0200

    add 14 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -895,3 +895,16 @@
 //   // console.log(values);
 // }
 
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+
+//   const keys = Object.keys(object);
+//   for (const key of keys) {
+//     propCount += 1;
+//   }
+
+//   return propCount;
+//   // Change code above this line
+// }
+
