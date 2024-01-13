74876e2e1181f9dca60f239b98fff263b346a422
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-13 21:58:40 +0200

    add 19 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -960,3 +960,24 @@
 //   return null;
 // }
 
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   const allName = [];
+
+//   for (const product of products) {
+//     for (const key in product) {
+//       if (key === propName) {
+//         allName.push(product[key]);
+//       }
+//     }
+//   }
+//   return allName;
+// }
+// console.log(getAllPropValues('name'));
+
