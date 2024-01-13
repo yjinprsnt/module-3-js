4e95858172259a48fab166ee2c181b7d8dbda5dc
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-13 22:13:50 +0200

    add 20 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -981,3 +981,23 @@
 // }
 // console.log(getAllPropValues('name'));
 
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   let totalPrise = 0;
+//   for (const product of products) {
+//     // console.log(product.name);
+//     if (product.name === productName) {
+//       totalPrise += product.price * product.quantity;
+//     }
+//   }
+//   return totalPrise;
+// }
+
+// calculateTotalPrice('Droid');
+
