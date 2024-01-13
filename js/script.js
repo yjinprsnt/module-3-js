e937b60d220558b4a50bb43062e892c43cbe165d
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-13 15:48:33 +0200

    add 17 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -929,3 +929,18 @@
 //   return totalSalary;
 // }
 
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+
