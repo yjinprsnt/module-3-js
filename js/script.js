59d579bd29503e61bc612c1adf2f72f1f61c516d
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-14 13:52:36 +0200

    add 29 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -1121,3 +1121,18 @@
 // const bestScore = Math.max(...allScores);
 // const worstScore = Math.min(...allScores);
 
+// const defaultSettings = {
+//   theme: 'light',
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Change code below this line
+// const finalSettings = { ...defaultSettings, ...overrideSettings };
+
