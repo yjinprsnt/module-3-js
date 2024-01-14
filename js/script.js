3999fb0e4e3116db2e45f42361c4baa6d0591c55
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-14 13:02:20 +0200

    add 22 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -1017,3 +1017,20 @@
 // // Change code above this line
 // const meanTemperature = (yesterday + today + tomorrow) / 3;
 
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const {
+//   yesterday,
+//   today,
+//   tomorrow,
+//   icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+// } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
