7f69c8b39002489cf5c88893b3d6bff235fec6a4
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-14 13:21:24 +0200

    add 24 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -1051,3 +1051,19 @@
 // // Change code above this line
 // const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
 
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { rgb, hex } of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
