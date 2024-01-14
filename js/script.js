46dcb78569fa8fd9e42fbc5dab43b55accfa9d16
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-14 13:52:14 +0200

    add 28 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -1109,3 +1109,15 @@
 // const bestScore = Math.max(...scores);
 // const worstScore = Math.min(...scores);
 
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [
+//   ...firstGroupScores,
+//   ...secondGroupScores,
+//   ...thirdGroupScores,
+// ];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
+
