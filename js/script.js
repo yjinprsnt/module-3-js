6f6f52ea717e5b6a05086cfb11f51be0e5f3862d
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-14 13:22:02 +0200

    add 25 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -1067,3 +1067,29 @@
 //   rgbColors.push(rgb);
 // }
 
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// const {
+//   today: {
+//     low: lowToday,
+//     high: highToday,
+//     icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+//   },
+//   tomorrow: {
+//     low: lowTomorrow,
+//     high: highTomorrow,
+//     icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+//   },
+// } = forecast;
+
