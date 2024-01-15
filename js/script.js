d4d83bf718e3ab78c0cb64ade73f85450248104b
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-15 19:35:43 +0200

    add 32 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -1164,3 +1164,21 @@
 // add(32, 6, 13, 19, 8);
 // add(74, 11, 62, 46, 12, 36);
 
+// // Change code below this line
+// function addOverNum(num, ...args) {
+//   let total = 0;
+
+//   for (const arg of args) {
+//     if (arg > num) {
+//       total += arg;
+//     }
+//   }
+
+//   return total;
+//   // Change code above this line
+// }
+// addOverNum(50, 15, 27);
+// addOverNum(10, 12, 4, 11, 48, 10, 8);
+// addOverNum(15, 32, 6, 13, 19, 8);
+// addOverNum(20, 74, 11, 62, 46, 12, 36);
+
