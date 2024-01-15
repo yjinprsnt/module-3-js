b7e08d513b112016e3e87e9f965d6fcf89706c3e
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-15 21:12:10 +0200

    add 37 auto check js module-3

--- js/script.js
+++ js/script.js
@@ -1236,3 +1236,12 @@
 //   // Change code above this line
 // };
 
+// const atTheOldToad = {
+//   potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+//   // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
+//   // Change code above this line
+// };
+
